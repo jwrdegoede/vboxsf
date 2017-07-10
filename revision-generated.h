e578f10886437ffd806865a3d7db6b7f21f56052
Hans de Goede <hdegoede@redhat.com>
2017-07-10 11:05:24 +0200

    Import vboxguest linux kernel module sources from upstream svn rev 67325
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

---
+++ revision-generated.h
@@ -0,0 +1,6 @@
+#ifndef __revision_generated_h__
+#define __revision_generated_h__
+
+#define VBOX_SVN_REV 67324
+
+#endif
---
+++ version-generated.h
@@ -0,0 +1,12 @@
+#ifndef ___version_generated_h___
+#define ___version_generated_h___
+
+#define VBOX_VERSION_MAJOR 5
+#define VBOX_VERSION_MINOR 1
+#define VBOX_VERSION_BUILD 51
+#define VBOX_VERSION_STRING_RAW "5.1.51"
+#define VBOX_VERSION_STRING "5.1.51"
+#define VBOX_API_VERSION_STRING "5_1"
+#define VBOX_PRIVATE_BUILD_DESC "Private build with export_modules"
+
+#endif
